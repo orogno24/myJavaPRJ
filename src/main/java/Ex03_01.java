95ad0e5a9629492413a38bcaab80fc0ed8ce44c5
orogno24 <orogno24@naver.com>
2023-03-15 11:46:37 +0900

    헬로5

--- src/main/java/Ex03_01.java
+++ src/main/java/Ex03_01.java
@@ -2,7 +2,11 @@ public class Ex03_01 {
     public static void main(String[] args) {
         System.out.printf("100+100");
         System.out.printf("\n");
+        //%d는 숫자를 의미함 밑의 코드는 100 + 100의 결과를 %d라는 글자로 출력하라, 라는 의미
         System.out.printf("%d", 100+100);
+        System.out.printf("\n");
+
+        System.out.printf("%d, %x, %f", 100, 20, 0.1f);
         System.out.printf("\n");
 
     }
