f7072e087b9d9cd0c9f72cd947aebb517e859500
orogno24 <orogno24@naver.com>
2023-05-29 21:09:17 +0900

    05 29 잔디심기

--- src/main/java/Quiz/Quiz_29.java
+++ src/main/java/Quiz/Quiz_29.java
@@ -4,20 +4,25 @@ import java.util.Scanner;
 
 public class Quiz_29 {
     public static void main(String[] args){
-        Scanner s = new Scanner(System.in);
-        int a;
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
-        System.out.printf("입력값 : ");
-        a = s.nextInt();
+        int output = 0;
 
-        if (a%3 == 0 && a%5 == 0){
-            System.out.printf("3의 배수와 5의 배수를 둘다 만족합니다.");
+        switch (input) {
+            case "ALPHA":
+                output = 1;
+                break;
+            case "BETA":
+                output = 2;
+                break;
+            case "CETA":
+                output = 3;
+                break;
+            default:
+                break;
         }
-        else if (a%3 == 0){
-            System.out.printf("3의 배수입니다.");
-        }
-        else if (a%5 == 0){
-            System.out.printf("5의 배수입니다.");
-        }
+
+        System.out.println(output);
     }
 }
