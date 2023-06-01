86041d75db8098a1513d5321c60ad011a20169d0
orogno24 <orogno24@naver.com>
2023-06-01 18:29:12 +0900

    06 01 잔디심기

---
+++ src/main/java/ZanDi06.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+
+public class ZanDi06 {
+    public static void main(String[] args) {
+
+        System.out.println("Quiz) DELETE, SELECT, DIVISION, JOIN 즁 순수 관계 연산자에 해당하지 않는 것을 대문자로 입력하시오 : ");
+
+        Scanner s = new Scanner(System.in);
+        String st = s.nextLine();
+
+        switch (st){
+            case "DELETE":
+                System.out.print("정답입니다.");
+                break;
+            case "SELECT":
+            case "DIVISION":
+            case "JOIN":
+                System.out.print("정답이 아닙니다.");
+                break;
+        }
+    }
+}
