ee489f6aff48102880a9f30dc9453e5567245d86
orogno24 <orogno24@naver.com>
2023-06-14 16:27:12 +0900

    잔디0614

--- src/main/java/ZanDi06/ZanDi0614.java
+++ src/main/java/ZanDi06/ZanDi0614.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 public class ZanDi0614 {
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
         Set<Integer> set = new HashSet<>();
 
