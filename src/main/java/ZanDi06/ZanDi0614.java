e0d9a9d51576327f70e515a121edf11168370599
orogno24 <orogno24@naver.com>
2023-06-14 16:26:18 +0900

    zandi0614

---
+++ src/main/java/ZanDi06/ZanDi0614.java
@@ -0,0 +1,24 @@
+package ZanDi06;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ZanDi0614 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Set<Integer> set = new HashSet<>();
+
+        System.out.print("input : ");
+        for(int i = 0; i < 5; i++){
+            int num = s.nextInt();
+            set.add(num);
+        }
+
+        System.out.print("output : ");
+        for(int num : set){
+            System.out.print(num + " ");
+
+        }
+    }
+}
