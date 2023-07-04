6cc0bc0c8b61c5a500df5e8a51081b95c81f4991
orogno24 <orogno24@naver.com>
2023-07-04 12:09:25 +0900

    잔디심기 0704

---
+++ src/main/java/Zandi07/Zandi0704.java
@@ -0,0 +1,12 @@
+package Zandi07;
+
+public class Zandi0704 {
+
+    private String user_id;
+    private String user_name;
+    private String password;
+    private String email;
+    private String addr1;
+    private String addr2;
+
+}
