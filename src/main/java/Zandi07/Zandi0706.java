12b4ac60b925afe1824d81a01bda6191f3481ef2
orogno24 <orogno24@naver.com>
2023-07-06 10:28:37 +0900

    잔디심기 0706

---
+++ src/main/java/Zandi07/Zandi0706.java
@@ -0,0 +1,19 @@
+package Zandi07;
+
+public class Zandi0706 {
+
+    static int[] arr(){
+        int a[] = new int[4];
+        int b = a.length;
+        for(int i = 0; i < b; i++)
+            a[i] = i;
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int a[] = arr();
+        for(int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+    }
+
+}
