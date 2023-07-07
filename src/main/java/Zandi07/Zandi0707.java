3d2fa8878adf31e3aad5ecf854139a0aa90a98b0
orogno24 <orogno24@naver.com>
2023-07-07 13:23:29 +0900

    잔디심기 07 07

---
+++ src/main/java/Zandi07/Zandi0707.java
@@ -0,0 +1,13 @@
+package Zandi07;
+
+public class Zandi0707 {
+    public static void main(String[] args) {
+        int i = 0, hap = 0;
+        do {
+            ++i;
+            hap += i;
+        } while (i < 5);
+        System.out.printf("%d, %d\n", i, hap);
+        }
+
+}
