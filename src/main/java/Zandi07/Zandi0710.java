eea5aec3a977c0ec1050678ec8bcc493549d8a12
orogno24 <orogno24@naver.com>
2023-07-07 13:23:29 +0900

    잔디심기 0710

---
+++ src/main/java/Zandi07/Zandi0710.java
@@ -0,0 +1,22 @@
+package Zandi07;
+
+
+import java.util.Scanner;
+
+public class Zandi0710 {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int a;
+
+        System.out.printf("정수를 입력하세요");
+        a = s.nextInt();
+
+        if(a % 3 == 0){
+            System.out.printf("5의 배수입니다.");
+        } else {
+
+        }
+
+    }
+}
