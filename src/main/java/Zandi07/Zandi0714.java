029b2c0331aedf47f46360709595a94bd9f231a6
orogno24 <orogno24@naver.com>
2023-07-14 16:08:12 +0900

    잔디 07 13

---
+++ src/main/java/Zandi07/Zandi0714.java
@@ -0,0 +1,24 @@
+package Zandi07;
+
+public class Zandi0714 {
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+        int b = 0, cont = 1, k = 1;
+
+        for(int a = 0; a < 5; a++) {
+            while (b <= 4 && b >= 0) {
+                arr[a][b] = k++;
+                b += cont;
+            }
+            cont *= -1;
+            b += cont;
+        }
+
+        for(int a = 0; a < 5; a++){
+            for(int i = 0; i < 5; i++){
+                System.out.printf("%3d", arr[a][i]);
+            }
+                System.out.println();
+        }
+    }
+}
