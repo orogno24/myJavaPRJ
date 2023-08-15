4d705d99ab067ef996a26e0cfa40e6970e3e7cb7
orogno24 <orogno24@naver.com>
2023-08-15 20:30:31 +0900

    잔디 08 15

---
+++ src/main/java/Zandi_08/Zandi0815.java
@@ -0,0 +1,37 @@
+package Zandi_08;
+
+import java.util.Scanner;
+
+public class Zandi0815 {
+
+    public void Triangle(int[] arr) {
+
+        if (arr[0] != 0 && arr[1] != 0 && arr[2] != 0) {
+            if (Math.pow(arr[0], 2) == Math.pow(arr[1], 2) + Math.pow(arr[2], 2)) {
+                System.out.print("right");
+            } else if (Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2)) {
+                System.out.println("right");
+            } else if (Math.pow(arr[0], 2) + Math.pow(arr[2], 2) == Math.pow(arr[1], 2)) {
+                System.out.println("right");
+            } else System.out.println("wrong");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        Zandi0815 zandi = new Zandi0815();
+
+        zandi.Triangle(arr);
+
+
+    }
+}
