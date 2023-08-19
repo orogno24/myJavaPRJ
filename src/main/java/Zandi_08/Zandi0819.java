139184c8be0835269c3ef6acf780e9534a8e122a
orogno24 <orogno24@naver.com>
2023-08-19 23:40:09 +0900

    잔디 08 19

---
+++ src/main/java/Zandi_08/Zandi0819.java
@@ -0,0 +1,16 @@
+package Zandi_08;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class Zandi0819 {
+
+    private String lat;
+    private String lon;
+    private double currentTemp;
+
+}
