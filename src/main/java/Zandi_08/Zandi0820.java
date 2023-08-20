9312dbbd6fb70a158ec93f640bd73c8a5a47ec6b
orogno24 <orogno24@naver.com>
2023-08-20 21:03:55 +0900

    잔디 08 20

---
+++ src/main/java/Zandi_08/Zandi0820.java
@@ -0,0 +1,20 @@
+package Zandi_08;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Zandi0820 {
+
+    private String day;
+    private String sunrise;
+    private String sunset;
+    private String moonrise;
+    private String moonset;
+    private String dayTemp;
+    private String dayTempMax;
+    private String dayTempMin;
+
+
+}
