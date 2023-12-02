cee6bf36c3b3c61fed5d325fc6d56aef61746bd0
orogno24 <orogno24@naver.com>
2023-12-02 23:08:14 +0900

    여기에 커밋 메시지를 입력하세요

--- src/main/java/CalculatorService.java
+++ src/main/java/CalculatorService.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class CalculatorService {
+public class Main {
+    public static void main(String[] args) {
+        CalculatorService calculatorService = new CalculatorService();
+        System.out.println(calculatorService.calculate(1, 1, '+'));
+    }
+
+}
+
+public class CalculatorService {
+    public int calculate(int a, int b, char operator) {
+        if (operator == '+') {
+            return a + b;
+        } else if (operator == '-') {
+            return a - b;
+        } else {
+            throw new
+        }
+
+    }
+
 }
--- src/main/java/CollectionOf.java
+++ src/main/java/CollectionOf.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class CollectionOf {
+import java.util.*;
+
+public class CollectionOf {
+
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("이협건");
+        set.add("홍길동");
+        set.add("고길동");
+        set.add("임꺽정");
+        set.add("둘리");
+
+        List<String> list = new ArrayList<>();
+        list.add("이협건");
+        list.add("홍길동");
+        list.add("고길동");
+        list.add("임꺽정");
+        list.add("둘리");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("이", "협건");
+        map.put("홍", "길동");
+        map.put("고", "길동");
+        map.put("임", "꺽정");
+        map.put("둘", "리");
+
+        System.out.println("set : " + set);
+        System.out.println("set : " + list);
+        System.out.println("set : " + map);
+
+    }
 }
