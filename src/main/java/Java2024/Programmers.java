c62fc7567e3b6c16c9b532fe2331a64d395c240e
Ongoro <orogno24@naver.com>
2024-10-22 22:03:44 +0900

    1022

---
+++ src/main/java/Java2024/Programmers.java
@@ -0,0 +1,31 @@
+package Java2024;
+
+import java.util.Arrays;
+
+public class Programmers {
+    public static void main(String[] args) {
+
+        int[] arr = {69, 10, 30, 2, 16, 8, 31, 22};
+        int[] arr2 = {69, 10, 30, 2, 16, 8, 31, 22};
+        int[] arr3 = {69, 10, 30, 2, 16, 8, 31, 22};
+
+        System.out.println(Arrays.toString(sortMethod(arr)));
+        System.out.println(Arrays.toString(sortMethod2(arr2)));
+        System.out.println(Arrays.toString(sortMethod3(arr3)));
+    }
+
+    public static int[] sortMethod(int[] arr) {
+
+        return arr;
+    }
+
+    public static int[] sortMethod2(int[] arr2) {
+
+        return arr2;
+    }
+
+    public static int[] sortMethod3(int[] arr3) {
+
+        return arr3;
+    }
+}
